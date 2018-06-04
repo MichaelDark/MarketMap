a97bc82fcbe95e1c766eac29a248ebfad68a14d8
Lyubchenko Yuriy <undeadspez@gmail.com>
2018-06-05 00:07:50 +0300

    Fix landing page

---
+++ prp/wwwroot/js/landing.js
@@ -0,0 +1,31 @@
+﻿(function () {
+    $('.transformable').addClass('t-hidden');
+
+    var nav = $('.nav'), navHeight = $('.nav').outerHeight(true), check;
+    $(nav).parent().css('padding-top', navHeight);
+
+    (check = function () {
+        var scrollOffset = $(window).scrollTop();
+        if (scrollOffset < navHeight)
+            $(nav).css('height', (navHeight - scrollOffset));
+        else
+            $(nav).css('height', '105px');
+
+        if (scrollOffset > (navHeight - 215)) {
+            nav.addClass('nav-fade');
+        }
+        else {
+            nav.removeClass('nav-fade');
+        }
+    })();
+
+    $(window).scroll(function () {
+        check();
+        $('.t-hidden').each(function () {
+            var $el = $(this);
+            if ($el.visible(true) && $el.hasClass('t-hidden')) {
+                $el.removeClass('t-hidden');
+            }
+        });
+    });
+})();
